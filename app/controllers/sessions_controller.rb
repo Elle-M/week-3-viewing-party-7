9cc443b205dd88aca75195276cea2b9c996df58b
Elle Majors <hellolovely@ellemajors.com>
2023-04-04 15:29:43 -0600

    add: sessions contoller, log out test, log out route and html in application

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class SessionsController < ApplicationController
+  before_action :set_current_user
+
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+end
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,35 +1,54 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "User Log In" do
-  it 'can log in a user with correct credentials' do
-    user = User.create(name: 'User One', email: 'email@example.com', password: 'password123')
+  it "can log in a user with correct credentials" do
+    user = User.create(name: "User One", email: "email@example.com", password: "password123")
 
     visit "/"
 
     click_link "Log In"
 
-    fill_in :email, with:'email@example.com'
-    fill_in :password, with: 'password123'
-    click_button 'Log In'
+    fill_in :email, with:"email@example.com"
+    fill_in :password, with: "password123"
+    click_button "Log In"
 
     expect(current_path).to eq("/users/#{user.id}")
     expect(page).to have_content("User One's Dashboard")
   end 
 
-  it 'does not log in user with incorrect credentials' do 
-    user = User.create(name: 'User One', email: 'email@example.com', password: 'password123')
+  it "does not log in user with incorrect credentials" do 
+    user = User.create(name: "User One", email: "email@example.com", password: "password123")
 
     visit "/"
 
     click_link "Log In"
 
-    fill_in :email, with:'email@example.com'
-    fill_in :password, with: 'nottherightpassword'
-    click_button 'Log In'
+    fill_in :email, with:"email@example.com"
+    fill_in :password, with: "nottherightpassword"
+    click_button "Log In"
 
 
     expect(current_path).to eq("/login")
     expect(page).to have_content("Bad Credentials, try again.")
-    
   end
+
+  it "can log out a user" do
+    user = User.create(name: "User One", email: "email@example.com", password: "password123")
+
+    visit "/"
+
+    click_link "Log In"
+
+    fill_in :email, with:"email@example.com"
+    fill_in :password, with: "password123"
+    click_button "Log In"
+
+    expect(current_path).to eq("/users/#{user.id}")
+    save_and_open_page
+    click_link "Log Out"
+
+    expect(current_path).to eq("/")
+    expect(page).to have_content("You are not logged in.")
+    expect(page).to have_link("Log In")
+  end  
 end
